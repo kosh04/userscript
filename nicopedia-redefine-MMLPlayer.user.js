3f8a11987a3ad39034876d90bd81fd01f015e9a7
KOBAYASHI Shigeru <shigeru.kb@gmail.com>
2014-11-13 02:15:00 +0900

    work for Firefox+Greasemonkey
    - use strict mode
    - cannot override global variable in strict mode.
    therefore, use HororeChuchuParero as local var.

--- nicopedia-redefine-MMLPlayer.user.js
+++ nicopedia-redefine-MMLPlayer.user.js
@@ -1,14 +1,20 @@
 // ==UserScript==
-// @name      ピコカキコプレーヤーの読み込み位置を正しくする
-// @namespace http://lambda.que.jp/
-// @version   0.1.20141112
-// @description  ニコニコ大百科 同じIDのピコカキコがページ上に複数ある場合に先頭のピコが再生されてしまう問題を修正する
-// @grant     GM_addStyle
-// @match     http://dic.nicovideo.jp/*
-// @author    kosh (mono)
-// @license   Public domain
+// @name         Nicopedia-Redefine-MMLPlayer
+// @namespace    http://lambda.que.jp/
+// @version      0.1.20141113
+// @description  ニコニコ大百科 ピコカキコプレーヤーの読み込み位置を正しくする
+// @grant        GM_addStyle
+// @match        http://dic.nicovideo.jp/*
+// @require      http://cdnjs.cloudflare.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require      http://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js
+// @require      http://jquery.thewikies.com/swfobject/jquery.swfobject.1-1-1.min.js
+// @author       kosh (mono)
+// @license      Public domain
 // ==/UserScript==
+
 /*
+同じIDのピコカキコがページ上に複数ある場合、先頭のピコが再生されてしまう問題を修正する。
+
 Before
 ------
 <div id="piko777">
@@ -24,10 +30,16 @@ After
 </div>
 */
 
-(function($, HororeChuchuParero) {
+(function($) {
+    "use strict";
+
+    // オブジェクトの擬似的な再定義
+    var HororeChuchuParero = {
+        flashVer: "2011090701"
+    };
+
     GM_addStyle(".pikoplayer { display: inline-block; }");
-    
-    // オブジェクト再定義
+
     HororeChuchuParero.MMLPlayer = {
         show_player: function(item) {
             var mml_id = $(item).attr("data-mml-id");
@@ -45,29 +57,31 @@ After
                 wmode: 'window',
                 hasVersion: '10.0.12',
                 expressInstaller: '/fla/expressInstall.swf',
+                flashvars: {
+                    autoPlay: 'y',
+                    mmlUrl: 'http://' + location.host + '/mml/' + mml_id,
+                    volume: $.cookie('volume')
+                },
                 params: {
                     allowScriptAccess: 'sameDomain',
-                    bgcolor: "#ffffff",
-                    flashvars: {
-                        autoPlay: 'y',
-                        mmlUrl: 'http://' + location.host + '/mml/' + mml_id,
-                        volume: $.cookie('volume')
-                    }
+                    bgcolor: "#ffffff"
                 }
             });
         }
     };
     
-    $('[id^="piko"]').each(function() {
+    $("[id^=piko]").each(function() {
         var $piko = $(this),
             mml_id = $piko.attr("id").substring(4), // id="piko777" to "777"
-            $pikoplayer = $("<div>").addClass("pikoplayer").attr({
-                "data-mml-id": mml_id,
-                "onclick": "HororeChuchuParero.MMLPlayer.show_player(this)"
-            });
-        // あるいは
-        // $pikoplayer.click(function() { HororeChuchuParero.MMLPlayer.show_player($piko); });
+            $pikoplayer = $("<div />")
+              .addClass("pikoplayer")
+              .attr("data-mml-id", mml_id)
+              // 元のオブジェクトを上書きしていないため、ここでは使えない
+              // .attr("onclick", "HororeChuchuParero.MMLPlayer.show_player(this)")
+              .click(function() {
+                  HororeChuchuParero.MMLPlayer.show_player(this);
+              });
         $piko.children().removeAttr("onclick");
         $piko.wrapInner($pikoplayer);
     });
-})(jQuery, HororeChuchuParero);
+})(jQuery);
