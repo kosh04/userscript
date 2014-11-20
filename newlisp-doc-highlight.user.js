4013a774cd4937759007ad14bd58785a935901fd
KOBAYASHI Shigeru <shigeru.kb@gmail.com>
2014-11-20 15:58:05 +0900

    code simplify

--- newlisp-doc-highlight.user.js
+++ newlisp-doc-highlight.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name      newLISP-Documentation-Highlight
 // @namespace http://lambda.que.jp/
-// @version   20141001
+// @version   20141120
 // @description  Provide syntax hightlighting in newLISP documentation
 // @grant   GM_addStyle
 // @grant   GM_getResourceText
@@ -19,21 +19,12 @@
 
 GM_addStyle([
     GM_getResourceText("zenburn.css"),
-    "pre code {",
-    "  font-size: 110%;",
-    "  font-family: Consolas, 'Courier New', Courier, Monaco, monospace;",
-    "}",
-    "span.arw {",
-    "  color: inherit;",
-    "}"
+    "pre code { font-size: 110%; font-family: Consolas, 'Courier New', Courier, Monaco, monospace; }",
+    "span.arw { color: inherit; }"
 ].join("\n"));
 
-$("pre").each(function(i, block) {
-    // wrap <pre></pre> to <pre><code></code></pre>
-    var code = $("<code></code>").html($(this).html());
-    code.addClass("lisp");
-    $(this).html(code);
-});
+var code = $("<code/>").addClass("lisp");
+$("pre").wrapInner(code);
 
 hljs.configure({languages:["lisp", "c", "bash"]});
 hljs.initHighlighting();
