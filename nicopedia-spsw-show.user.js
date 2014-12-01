3a19eda529cc03ac609ce0ba421bc0dbb176ba54
KOBAYASHI Shigeru <shigeru.kb@gmail.com>
2014-12-02 01:12:28 +0900

    ignore /t/*

--- nicopedia-spsw-show.user.js
+++ nicopedia-spsw-show.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         ニコニコ大百科 スマートフォン版リンクを常に表示する
 // @namespace    http://lambda.que.jp/
-// @version      0.1.20141112
+// @version      0.1.20141202
 // @description  ニコニコ大百科(通常版)の右メニューにある「スマートフォン版で見る」リンクを常に表示する
 // @grant        none
 // @match        http://dic.nicovideo.jp/*
+// @exclude      http://dic.nicovideo.jp/t/*
 // @author       kosh (mono)
 // @license      Public domain
 // ==/UserScript==
