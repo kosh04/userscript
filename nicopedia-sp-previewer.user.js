311cfa096ea7cc95671a17f28a3b660f7b599b92
KOBAYASHI Shigeru <shigeru.kb@gmail.com>
2018-04-06 07:00:26 +0900

    Follow up the redesigned website
    - http://ch.nicovideo.jp/pedia/blomaga/ar1463398
    - use modern js syntax

--- nicopedia-sp-previewer.user.js
+++ nicopedia-sp-previewer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nicopedia-Smartphone-Previewer
 // @namespace    http://lambda.que.jp/
-// @version      0.1.20160116
+// @version      0.1.20180406
 // @description  ニコニコ大百科の記事編集ページにスマートフォン版のプレビューを追加する
 // @grant        GM_addStyle
 // @match        http://dic.nicovideo.jp/p/a/*
@@ -24,68 +24,87 @@
 (function($) {
     "use strict";
 
-    var $article = $('#nicopedia-article-textarea');
+    const $article = $('#nicopedia-article-textarea');
 
     if ($article.length === 0) {
         return;
     }
 
-    GM_addStyle([
-        '.sp-window { padding: 5px; }',
-        '.sp-window h2 { font-size: 1.3em; border-bottom: double 1px #8cc700; margin: 0.4em 0; }',
-        '.resizable { overflow: auto; resize: both; }',
-        '.sp-view { background-color: #fff; border: solid 1px #66a32f; margin: 10px auto; }',
-        '.sp-view iframe { overflow: hidden; width: 100%; height: 100%; }',
-        '#sp-view { width: 320px; height: 480px; }'
-    ].join("\n"));
+    GM_addStyle(`
+.sp-window { padding: 5px; }
+.sp-window h2 { font-size: 1.3em; border-bottom: double 1px #8cc700; margin: 0.4em 0; }
+.resizable { overflow: auto; resize: both; }
+.sp-view { background-color: #fff; border: solid 1px #66a32f; margin: 10px auto; }
+.sp-view iframe { overflow: hidden; width: 100%; height: 100%; }
+#sp-view { width: 375px; height: 667px; }
+`);
 
-    var html = [
-        '<div class="left-box" style="margin: 5px 0; background-image: none; border: solid 1px #aaa; border-radius: 5px;">',
-        '  <div class="sp-window">',
-        '    <h2>スマホ版プレビュー</h2>',
-        '    <button class="sp-rotate-button" type="button">画面の回転</button>',
-        '    <button class="sp-resize-button" type="button" data-width="320" data-height="480">320x480 (iPhone 4S)</button>',
-        '    <button class="sp-resize-button" type="button" data-width="600" data-height="960">600x960 (Nexus 7)</button>',
-        '    <div id="sp-view" class="sp-view">',
-        '      <iframe srcdoc=""></iframe>',
-        '    </div>',
-        '  </div>',
-        '</div>'
-    ].join("\n");
+    // デバイスサイズは以下のリンク参照
+    // https://material.io/devices/
+    // https://developer.apple.com/library/content/documentation/DeviceInformation/Reference/iOSDeviceCompatibility/Displays/Displays.html
+    const html = `
+<div class="left-box" style="margin: 5px 0; background-image: none; border: solid 1px #aaa; border-radius: 5px;">
+<div class="sp-window">
+<h2>スマホ版プレビュー</h2>
+<p>※実際の表示とは異なる場合があります</p>
+<button class="sp-rotate-button" type="button">画面の回転</button>
+<select id="sp-device-list">
+<option data-width="320" data-height="480">320x480 (iPhone 4S)</option>
+<option data-width="375" data-height="667" selected>375x667 (iPhone 6/7/8)</option>
+<option data-width="600" data-height="960">600x960 (Nexus 7)</option>
+<option data-width="768" data-height="1024">768x1024 (iPad)</option>
+</select>
+<div id="sp-view" class="sp-view">
+<iframe srcdoc=""></iframe>
+</div>
+</div>
+</div>
+`;
+    const srcdoc = `
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+<link rel="stylesheet" href="/css/reset.css">
+<link rel="stylesheet" href="/css/style.css?s=104">
+</head>
+<body>
+<div id="top" class="bs-Wrapper">
+<div class="bs-Wrapper_Inner">
+<section class="sw-Article">
+<div class="sw-Article_TitleBox"><!-- 記事名 --></div>
+<div class="sw-Article_Menu"><!-- ほめる/共有/その他 --></div>
+<div class="sw-Article_Body">
+<div class="sw-Article_Body-inner">
+<span class="article">${$article.text()}</span>
+</div>
+</div>
+</section>
+</div>
+</div>
+</body>
+</html>
+`;
 
     $("#article-tab-nico").before(html);
     // $("#sp-view").addClass("resizable");
-    $("#sp-view > iframe").attr("srcdoc", [
-        '<!DOCTYPE html><html><head><meta charset="utf-8" />',
-        '<meta name="viewport" content="width=device-width" />',
-        '<link rel="stylesheet" href="/main_sp.css?s=3" type="text/css" />',
-        '</head>',
-        '<body>',
-        '<div class="article">',
-        $article.text(),
-        '</div>',
-        '</body>',
-        '</html>'
-    ].join("\n"));
+    $("#sp-view > iframe").attr("srcdoc", srcdoc);
 
-    $(".sp-rotate-button").click(function() {
-        var $view = $("#sp-view"),
-            width = $view.css("width"),
-            height = $view.css("height");
+    const $view = $("#sp-view");
+
+    $(".sp-rotate-button").click(() => {
         $view.css({
-            "width": height,
-            "height": width
+            width:  $view.css("height"),
+            height: $view.css("width")
         });
     });
-    $(".sp-resize-button").each(function() {
-        var $this = $(this),
-            width = $this.attr("data-width"),
-            height = $this.attr("data-height");
-        $this.click(function() {
-            $("#sp-view").css({
-                "width": width + "px",
-                "height": height + "px"
-            });
+
+    $("#sp-device-list").change((e) => {
+        const data = e.target[e.target.selectedIndex].dataset;
+        $view.css({
+            width:  `${data.width}px`,
+            height: `${data.height}px`
         });
     });
 
