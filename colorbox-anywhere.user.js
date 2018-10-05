10884cd7b631c771d505ee88e2922dfbc7e0c1c3
kosh04 <shigeru.kb@gmail.com>
2018-10-05 22:10:16 +0900

    update
    like a modern JS syntax

--- colorbox-anywhere.user.js
+++ colorbox-anywhere.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         colorbox-anywhere
 // @namespace    https://github.com/kosh04/userscript
-// @version      0.1.20171208
+// @version      0.20180930
 // @description  Colorbox (a jQuery lightbox plugin) with any web page.
+// @author       KOBAYASHI Shigeru (kosh)
 // @grant        GM_getResourceText
 // @grant        GM_getResourceURL
 // @grant        GM_addStyle
@@ -17,40 +18,24 @@
 // @resource     images/loading.gif   https://cdnjs.cloudflare.com/ajax/libs/jquery.colorbox/1.4.33/example1/images/loading.gif
 // @resource     images/loading_background.png  https://cdnjs.cloudflare.com/ajax/libs/jquery.colorbox/1.4.33/example1/images/loading_background.png
 // @resource     images/overlay.png   https://cdnjs.cloudflare.com/ajax/libs/jquery.colorbox/1.4.33/example1/images/overlay.png
-// @author       KOBAYASHI Shigeru (kosh)
-// @copyright    2015-2017 kosh
+// @noframes
 // ==/UserScript==
 
 // TODO
 // - Avoid conflict other lightbox plugin
 
-function escapeRegExp(string) {
-    var re = new RegExp("([.*+?^=!:${}()|[\]/\\\\])", "g");
-    return string.replace(re, "\\$1");
-}
+/* global jQuery, $ */
 
-function enable_colorbox() {
-    var css = GM_getResourceText("colorbox.css"),
-        resources = [
-            { from: "images/border.png",   to: GM_getResourceURL("images/border.png")},
-            { from: "images/controls.png", to: GM_getResourceURL("images/controls.png") },
-            { from: "images/loading.gif",  to: GM_getResourceURL("images/loading.gif") },
-            { from: "images/loading_background.png", to: GM_getResourceURL("images/loading_background.png") },
-            { from: "images/overlay.png",  to: GM_getResourceURL("images/overlay.png") }
-        ];
-
-    for (var i = 0; i < resources.length; i++) {
-        var from = new RegExp(escapeRegExp(resources[i].from), "g"),
-            to = resources[i].to;
-        css = css.replace(from, to);
-    }
+GM_registerMenuCommand("Enable Colorbox", function() {
+    let css = GM_getResourceText("colorbox.css");
+    css = css.replace(/images\/\w+\.(?:png|gif)/g, (match, _offset, _string) => {
+        return GM_getResourceURL(match) || match;
+    });
 
     GM_addStyle(css);
 
-    var $a = $("a").filter(function() {
-        var path = new URL(this.href).pathname; // trim query string
-        return path.match(/\.(jpe?g|png|gif)$/i);
-    });
+    let $a = $("a").filter((_, elm) => elm.pathname.match(/\.(jpe?g|png|gif)$/i));
+
     alert(`Found ${$a.length} items.`);
 
     $a.colorbox({
@@ -58,14 +43,12 @@ function enable_colorbox() {
         height: "100%",
         slideshow: false,
         title: function() {
-            var a = $(this);
-            var img  = a.find("img");
+            let a = $(this);
+            let img = a.find("img");
             return img.attr("title") ||
                 img.attr("alt") ||
                 img.attr("src") ||
                 a.text();
-        }
+      }
     });
-}
-
-GM_registerMenuCommand("Enable Colorbox", enable_colorbox);
+});
--- newlisp-doc-highlight.user.js
+++ newlisp-doc-highlight.user.js
@@ -1,32 +1,44 @@
 // ==UserScript==
-// @name      newLISP-Documentation-Highlight
-// @namespace http://lambda.que.jp/
-// @version   20141120
+// @name         newLISP-Documentation-Highlight
+// @namespace    https://github.com/kosh04/userscript
+// @version      0.20181005
 // @description  Provide syntax hightlighting in newLISP documentation
-// @grant   GM_addStyle
-// @grant   GM_getResourceText
-// @match   http://www.newlisp.org/*/newlisp_manual.html
-// @match   http://www.newlisp.org/*/CodePatterns.html
-// @match   http://newlisp.nfshost.com/*/newlisp_manual.html
-// @match   http://newlisp.nfshost.com/*/CodePatterns.html
-// @require http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js
-// @require http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.2/highlight.min.js
-// @require http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.2/languages/lisp.min.js
-// @resource default.css http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.2/styles/default.min.css
-// @resource github.css  http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.2/styles/github.min.css
-// @resource zenburn.css http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.2/styles/zenburn.min.css
-// @author KOBAYASHI Shigeru (kosh)
-// @license Public domain
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @match        http://www.newlisp.org/*/newlisp_manual.html
+// @match        http://www.newlisp.org/*/CodePatterns.html
+// @match        http://newlisp.nfshost.com/*/newlisp_manual.html
+// @match        http://newlisp.nfshost.com/*/CodePatterns.html
+// @require              http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js
+// @require              http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/languages/lisp.min.js
+// @resource default.css http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css
+// @resource github.css  http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/github.min.css
+// @resource zenburn.css http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/zenburn.min.css
+// @author       KOBAYASHI Shigeru (kosh)
+// @license      Public domain
 // ==/UserScript==
 
-GM_addStyle([
-    GM_getResourceText("zenburn.css"),
-    "pre code { font-size: 110%; font-family: Consolas, 'Courier New', Courier, Monaco, monospace; }",
-    "span.arw { color: inherit; }"
-].join("\n"));
+/* global hljs */
 
-var code = $("<code/>").addClass("lisp");
-$("pre").wrapInner(code);
+GM_addStyle(GM_getResourceText("default.css"));
+GM_addStyle(`
+code {
+  /* font-size: 110%; */
+  font-family: Consolas, 'Courier New', Courier, Monaco, monospace;
+}
+.arw {
+  color: inherit;
+}
+`);
+
+document.querySelectorAll("pre").forEach(pre => {
+    const code = document.createElement("code");
+    code.classList.add("lisp"); // assume lang=lisp
+
+    // wrapInner: <pre>...</pre> => <pre><code>...</code></pre>
+    code.innerHTML = pre.innerHTML;
+    pre.innerHTML = code.outerHTML;
+});
 
 hljs.configure({languages:["lisp", "c", "bash"]});
 hljs.initHighlighting();
