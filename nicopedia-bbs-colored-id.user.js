1a26095d2ba1c13192339a92398deb9424087465
kosh04 <shigeru.kb@gmail.com>
2019-03-08 05:13:21 +0900

    ニコ百のデザインリニューアルに追従
    - https://blog.nicovideo.jp/niconews/103393.html
    - ついでに文法を少しモダンに修正

--- nicopedia-bbs-colored-id.user.js
+++ nicopedia-bbs-colored-id.user.js
@@ -1,39 +1,45 @@
 // ==UserScript==
 // @name         Nicopedia-BBS-Colored-ID
 // @namespace    https://github.com/kosh04/userscript
-// @version      0.20181005
-// @description  ニコニコ大百科掲示板のid表記を色分けする
+// @updateURL    https://github.com/kosh04/userscript/raw/master/nicopedia-bbs-colored-id.user.js
+// @version      0.20190308
+// @description  ニコニコ大百科掲示板のID表記を色分けする
 // @author       kosh (mono)
 // @match        http://dic.nicovideo.jp/*
 // @match        https://dic.nicovideo.jp/*
 // @grant        none
 // @noframes
-// ==/userscript==
+// ==/UserScript==
+
+// ChangeLog
+// =========
+// * 0.20190308 PC版サイトのデザインリニューアルに伴うCSSセレクタの修正
+// * 0.20181005 初版
 
 // https://stackoverflow.com/a/7616484/4499880
-string.prototype.hashcode = function() {
+String.prototype.hashCode = function() {
     var hash = 0, i, chr;
     if (this.length === 0) return hash;
     for (i = 0; i < this.length; i++) {
-        chr   = this.charcodeat(i);
+        chr   = this.charCodeAt(i);
         hash  = ((hash << 5) - hash) + chr;
-        hash |= 0; // convert to 32bit integer
+        hash |= 0; // Convert to 32bit integer
     }
     return hash;
 };
 
 function dechex(number) {
     if (number < 0) {
-        number = 0xffffffff + number + 1;
+        number = 0xFFFFFFFF + number + 1;
     }
-    return parseint(number, 10).tostring(16);
+    return parseInt(number, 10).toString(16);
 }
 function id2rgb(id) {
-    return "#" + ("000000" + dechex(id.hashcode())).slice(-6);
+    return "#" + ("000000" + dechex(id.hashCode())).slice(-6);
 }
 
-[].foreach.call(document.queryselectorall(".reshead"), (reshead) => {
-    reshead.innerHTML = reshead.innerHTML.replace(/ID: ([A-Za-z0-9+\/]{10})/, (match_, id, _offset, _string_) => {
+document.querySelectorAll(".st-bbs_resInfo").forEach(reshead => {
+    reshead.innerHTML = reshead.innerHTML.replace(/ID: ([A-Za-z0-9+\/]{10})/, (match_, id, offset_, _string_) => {
         const rgb = id2rgb(id);
         return `ID: <span style="background: linear-gradient(transparent 85%, ${rgb} 0%);" data-id="${id}">${id}</span>`;
     });
--- nicopedia-resanchor-popup.user.js
+++ nicopedia-resanchor-popup.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name      Nicopedia-Resanchor-Popup
 // @namespace https://github.com/kosh04/userscript
-// @version   0.20170604
+// @version   0.20190308
 // @description  ニコニコ大百科 掲示板のレスアンカーをポップアップ表示する
 // @grant     GM_addStyle
 // @grant     GM_getResourceURL
@@ -14,63 +14,84 @@
 // @resource  images/ui-bg_flat_75_ffffff_40x100.png https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.4/css/images/ui-bg_flat_75_ffffff_40x100.png
 // @author    kosh (mono)
 // @license   Public domain
+// @noframes
 // ==/UserScript==
 
-var css = GM_getResourceText("jquery-ui.css")
-.replace("images/ui-bg_flat_75_ffffff_40x100.png", GM_getResourceURL("images/ui-bg_flat_75_ffffff_40x100.png"));
-GM_addStyle([
-    css,
-    ".ui-tooltip {",
-    "  min-width: 600px;",
-    "  font-size: small;",
-    "  line-height: 1.3em;",
-    "  text-align: left;",
-    "}"
-].join("\n"));
+// ChangeLog
+// =========
+// * 0.20190308 PC版サイトのデザインリニューアルに伴うCSSセレクタの修正
+// * 0.20141021 初版
+
+/* global jQuery, $ */
+
+// https://stackoverflow.com/a/17606289/4499880
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
+
+let css = GM_getResourceText("jquery-ui.css");
+[
+    "images/ui-bg_flat_75_ffffff_40x100.png",
+].forEach(resource => {
+    css = css.replaceAll(resource, GM_getResourceURL(resource));
+});
+GM_addStyle(css);
+GM_addStyle(`
+.ui-tooltip {
+  min-width: 600px;
+  font-size: small;
+  line-height: 1.3em;
+  text-align: left;
+}
+`);
 
 function getAnchorContent(id, context) {
-    var $anchor, doc = document;
+    let doc = document;
     // jQueryによるHTML解析は<script>が実行されるらしい
     // var $anchor = $(context || document).find("a[name=" + id + "]");
 
     if (typeof context === "string") {
-        var parser = new DOMParser();
+        let parser = new DOMParser();
         doc = parser.parseFromString(context, "text/html");
     }
-    $anchor = $(doc).find("a[name=" + id + "]");
+
+    let $anchor = $(doc).find(`a[name=${id}]`);
     if ($anchor.length === 0) {
         return null;
     }
 
-    var $parent = $anchor.parent();
-    return "<dl>" + /*dt*/ $parent[0].outerHTML + /*dd*/ $parent.next()[0].outerHTML + "</dl>";
+    let $parent = $anchor.parent();
+    let dt = $parent[0].outerHTML;
+    let dd = $parent.next()[0].outerHTML;
+    return `<dl>${dt}${dd}</dl>`;
 }
 
-$("dd.resbody a.dic").filter(function() {
+$(".st-bbs_resbody a.dic").filter(function() {
     return this.hash.match(/^#[0-9]+$/);
 }).tooltip({
     items: "[href]",
     content: "読み込み中",
     open: function(event, ui) {
-        var $elem = $(this),
-            url = this.href,
-            anchor_id = this.hash.substring(1),
-            content = getAnchorContent(anchor_id),
-            popup = function(content) {
-                $elem.tooltip("option", "content", content);
-            };
+        let $elem = $(this);
+        let url = this.href;
+        let anchor_id = this.hash.substring(1);
+        let content = getAnchorContent(anchor_id);
+        let popup = function(content) {
+            $elem.tooltip("option", "content", content);
+        };
         if (content && !content.includes("全て読むにはこのリンクをクリック！")) {
             // console.log("同じページ");
             popup(content);
         } else {
             // console.log("別のページ");
             $.get(url, null, function(data) {
-                var content = getAnchorContent(anchor_id, data) || "見つかりません";
+                let content = getAnchorContent(anchor_id, data) || "見つかりません";
                 popup(content);
             });
         }
     },
-/*
+    /*
     // 表示位置をカーソルの右上にするとレスと被らなくて見やすい
     // ただし、カーソルのフォーカスと表示が引っかかる気がする
     position: {
