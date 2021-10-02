9afadaddadb174592bf6116efc1a746e20f68839
kosh04 <shigeru.kb@gmail.com>
2021-10-02 18:09:43 +0900

    Update/Workaround about flmml v2.x

--- nicopedia-flmml-on-html5.user.js
+++ nicopedia-flmml-on-html5.user.js
@@ -1,43 +1,59 @@
 // ==UserScript==
 // @name        Nicopedia-FlMMLonHTML5
-// @namespace   https://github.com/kosh04/FlMMLonHTML5
-// @version     0.20181110
+// @namespace   https://github.com/kosh04/userscript
+// @version     0.20211002
 // @description ニコニコ大百科のピコカキコプレーヤーをFlMMLonHTML5に置き換える (デバッグ用)
-// @grant       GM_getResourceText
-// @match       http://dic.nicovideo.jp/*
 // @match       https://dic.nicovideo.jp/*
-// @resource    flmmlworker.js  https://argentum384.github.io/flmml-on-html5/flmmlworker.js
+// @require     https://cdn.jsdelivr.net/npm/flmml-on-html5@2.0.0/dist/flmml-on-html5.js
+// @require     https://ghcdn.rawgit.org/argentum384/flmml-on-html5/v1.2.0/src/flmmlplayer-raw.js
+// @resource    flmmlworker.js  https://cdn.jsdelivr.net/npm/flmml-on-html5@2.0.0/dist/flmml-on-html5.worker.js
+// @grant       GM_getResourceText
+// @grant       GM_registerMenuCommand
+// @grant       unsafeWindow
 // @noframes
 // @author      kosh (mono)
 // ==/UserScript==
 
-function loadScript(src) {
-    var s = document.createElement("script");
-    s.type = "text/javascript";
-    s.src = src;
-    document.head.appendChild(s);
-}
+/*global FlMML,FlMMLPlayer*/
 
-// NOTE: バージョンは flmmlonhtml5.js のヘッダーコメント参照
-loadScript("https://argentum384.github.io/flmml-on-html5/flmmlonhtml5.js");
-loadScript("https://argentum384.github.io/flmml-on-html5/flmmlplayer.js");
+'use strict';
 
-const code = GM_getResourceText("flmmlworker.js");
-const blob = new Blob([code], { type: "text/javascript" });
-const workerURL = URL.createObjectURL(blob);
+// XXX: FlMMLPlayer@1.x から FlMML@2.x を呼び出すための回避策
+// v2.0.0 リリースログから引用
+// >>> エントリポイント FlMMLonHTML5 の名前を FlMML に変更。ただし当面の間 FlMMLonHTML5 でも利用可
+unsafeWindow.FlMMLonHTML5 = FlMML;
 
-document.querySelectorAll("[id^=piko]").forEach(piko => {
-    const m = piko.id.match(/^piko(?:bbs|list)?(\d+)/);
+// XXX: CORS 対策
+const workerURL = (() => {
+    const code = GM_getResourceText("flmmlworker.js");
+    const blob = new Blob([code], { type: "application/javascript" });
+    return URL.createObjectURL(blob);
+})();
+
+// セレクタ指定ヒント
+// id="piko12345"
+// id="piko124_u2" (単語記事「ピコカキコ」参照)
+// id="pikobbs12345"
+// id="pikolist12345"
+// id="pikosearch12345"
+const pikoSelector = '[id^=piko]';
+
+document.querySelectorAll(pikoSelector).forEach(piko => {
+    const m = piko.id.match(/^piko(?:bbs|list|search)?(\d+)/);
     if (!m) return;
-    const mml_id = m[1];        // "piko777" -> "777"
-    const option = JSON.stringify({
-      mmlURL: `/mml/${mml_id}`,
-      height: "1.65em",
-      underground: true,
-      workerURL: workerURL
-    });
-    const code = `(function(elem) { var player = new FlMMLPlayer(${option}); elem.parentNode.replaceChild(player.svg, elem); })(this)`;
+    const mml_id = m[1]; // "piko777" -> "777"
 
     const imgPikoplayer = piko.children.item(0);
-    imgPikoplayer.setAttribute("onclick", code);
+    imgPikoplayer.removeAttribute("onclick"); // 元イベントの無効化
+    imgPikoplayer.addEventListener('click', (e) => {
+        const player = new FlMMLPlayer({
+            mmlURL: `/mml/${mml_id}`,
+            height: "1.8em",
+            underground: true,
+            workerURL: workerURL,
+        });
+        e.target.parentNode.replaceChild(player.svg, e.target);
+    });
 });
+
+FlMML.prepare(pikoSelector);
